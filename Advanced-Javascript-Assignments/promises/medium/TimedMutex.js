36fe9e8cc5aebd60d94c293c509d101c113b2149
im1-omkar <robintheconsistent21@gmail.com>
2026-02-18 22:22:21 +0530

    more question solved in medium range

--- Advanced-Javascript-Assignments/promises/medium/TimedMutex.js
+++ Advanced-Javascript-Assignments/promises/medium/TimedMutex.js
@@ -7,21 +7,55 @@
 // This problem tests concurrency control and timeout handling.
 //
 
-class TimedMutex {
+ class TimedMutex {
   constructor() {
     this.locked = false;
     this.queue = [];
-
   }
 
-  acquire(timeoutMs) {
-      if(this.locked == true){
-        this.queue.push(timeoutMs)
+  acquire(timeout) {
+    return new Promise((resolve, reject) => {
+      
+      if (!this.locked) {
+        this.locked = true;
+        resolve(this._createRelease());
+        return;
       }
 
-      return ()=>{
+      
+      const request = {};
+
+      request.resolve = resolve;
+      request.reject = reject;
+
+     
+      request.timeoutId = setTimeout(() => {
+        
+        const index = this.queue.indexOf(request);
+        if (index !== -1) {
+          this.queue.splice(index, 1);
+        }
+        reject("Lock Timeout");
+      }, timeout);
+
+      this.queue.push(request);
+    });
+  }
+
+  _createRelease() {
+    return () => {
+      if (this.queue.length > 0) {
+        const next = this.queue.shift();
+
+        
+        clearTimeout(next.timeoutId);
+
+        
+        next.resolve(this._createRelease());
+      } else {
         this.locked = false;
       }
+    };
   }
 }
 
