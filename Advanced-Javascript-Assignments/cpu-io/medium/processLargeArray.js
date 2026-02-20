488170acd241aa3c9f5cede27f2526ba691ae788
im1-omkar <robintheconsistent21@gmail.com>
2026-02-20 23:43:20 +0530

    all medium level questions are completed

--- Advanced-Javascript-Assignments/cpu-io/medium/processLargeArray.js
+++ Advanced-Javascript-Assignments/cpu-io/medium/processLargeArray.js
@@ -3,6 +3,27 @@
 // You are given a very large array containing around 100,000 items that must be processed. 
 // Your task is to implement a strategy that performs this processing without blocking the main thread, ensuring the browser UI remains responsive. 
 // The solution should break the work into smaller chunks and schedule them asynchronously.
-async function processLargeArray(items, processFn) {}
+
+        /**DONE */
+
+async function processLargeArray(items, processFn) {
+    
+    let n = items.length
+    for(let i=0; i<n; i++){
+        try{
+            processFn(items[i])
+
+            if(i%100 ==0){
+                await new Promise((resolve, reject)=>{
+                    setImmediate(resolve)
+                })
+            }
+        }
+        catch(err){
+            throw err
+        }
+    }
+
+}
 
 module.exports = processLargeArray;
--- Advanced-Javascript-Assignments/cpu-io/medium/retryWithJitter.js
+++ Advanced-Javascript-Assignments/cpu-io/medium/retryWithJitter.js
@@ -4,6 +4,34 @@
 // On each retry, the delay before the next attempt should increase, and a small random “jitter”
 // should be added to the delay to prevent synchronized retries that can overload a server. 
 // The process should stop once the task succeeds or the maximum retry limit is reached.
-async function retryWithJitter(fn, retries = 3, baseDelay = 1000) {}
+
+    /**DONE */
+    
+async function retryWithJitter(fn, retries = 3, baseDelay = 1000) {
+
+    return new Promise((resolve, reject)=>{
+
+        async function fetch(fn, retries, baseDelay, depth){
+            //base case
+            if(depth > retries){
+                return reject(new Error("always fails"))
+            }
+
+            try{
+                const data = await fn()
+                resolve(data)
+                return;
+            }
+            catch(err){
+                await new Promise((res)=>{setTimeout(()=>{res()},baseDelay)})
+                return fetch(fn, retries, baseDelay, depth+1)
+            }
+            
+        }
+
+        fetch(fn, retries, baseDelay,0)
+    })
+
+}
 
 module.exports = retryWithJitter;
