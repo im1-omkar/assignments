f2e59ec8b91787823f13f953b146c3debca39ee9
im1-omkar <robintheconsistent21@gmail.com>
2026-06-05 00:06:07 +0530

    recharts are created

---
+++ Combined-assignment/Week-06/easy/test/src/App.tsx
@@ -0,0 +1,18 @@
+// App.tsx
+
+import Step2 from "./component/chart"
+import Step1 from "./component/chart"
+
+
+
+const App = () => {
+  
+
+  return (
+    <div style={{border:"2px solid black"}}>
+      <Step2/>
+    </div>
+  )
+}
+
+export default App
